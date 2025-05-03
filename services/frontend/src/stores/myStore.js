3c08e84b26190a8e9e11fbd8815b434dda735331
mounif <mouniftormos@gmail.com>
2025-05-03 12:04:40 -0400

    [FEATURE] Backend simples em mySql

--- services/frontend/src/stores/myStore.js
+++ services/frontend/src/stores/myStore.js
@@ -23,6 +23,9 @@ export const useMyStore = defineStore('myStore', {
       return n_items
     },
     filteredItems: (state) => {
+      if (!Array.isArray(state.items)) {
+        return []
+      }
       return state.items.filter(item => {
         const matchesCategory = state.selectedCategories.length === 0 ||
                                 state.selectedCategories.includes('todas') ||
@@ -31,7 +34,7 @@ export const useMyStore = defineStore('myStore', {
         const matchesSearch = !state.search ||
                                       item.title.toLowerCase().includes(state.search.toLowerCase())
         return matchesCategory && matchesSearch
-      });
+      })
     }
   },
 
@@ -53,8 +56,11 @@ export const useMyStore = defineStore('myStore', {
       this.loading = true
       this.error = null
       try {
-        const response = await axios.get('http://127.0.0.1:8001/items/') // use your actual endpoint
+        const response = await axios.get('http://127.0.0.1:8500/items/') // use your actual endpoint
+        // For postgres
         this.items = response.data
+        // For mySql
+        this.items = response.data.items
       } catch (err) {
         this.error = 'Failed to fetch items'
         console.error(err)
